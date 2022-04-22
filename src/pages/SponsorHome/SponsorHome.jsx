c8e662c275c8bc2e70e84fe758306a0a35c586da
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-22 18:06:39 -0300

    feat: create Sponsor home page and integrate it

--- src/pages/SponsorHome/SponsorHome.jsx
+++ src/pages/SponsorHome/SponsorHome.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../components/UserContext/UserContext";
+import api from "../../services/api";
+
+const getSponsorshipStatus = (accept) =>
+  ({ [true]: "Aceito", [false]: "Recusado", [null]: "Não decidiu" }[accept]);
 
 const SponsorHome = () => {
-  return <h1>Sponsor home</h1>;
+  const {
+    userData: { access_token, account },
+  } = React.useContext(UserContext);
+
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await api.get(`/match/sponsors/${account.id}`, {
+        headers: {
+          authorization: `Bearer ${access_token}`,
+        },
+      });
+      setUsers(response.data);
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h1>Home - Sponsor</h1>
+      <ul>
+        {users.length !== 0 &&
+          users.map((user) => (
+            <li key={user.id}>
+              <p>Nome: {`${user.accounts_accountsTomatch_idUser.name}`}</p>
+              <p>Status: {getSponsorshipStatus(user.accept)}</p>
+              <p>
+                <Link to={`candidatos/${user.id}`}>booora</Link>
+              </p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 
 export default SponsorHome;
