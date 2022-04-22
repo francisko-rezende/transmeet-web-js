2badb05ff38d5922851dd2ee90008dc874eaa9ba
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-22 16:21:32 -0300

    feat: create user home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../components/UserContext/UserContext";
+import api from "../../services/api";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    userData: { access_token, account },
+  } = React.useContext(UserContext);
+
+  const [sponsors, setSponsors] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchSponsors = async () => {
+      const response = await api.get("/sponsors", {
+        headers: {
+          authorization: `Bearer ${access_token}`,
+        },
+      });
+      setSponsors(response.data);
+    };
+
+    if (account.typeId === 1) {
+      fetchSponsors();
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Home</h1>
+      <ul>
+        {sponsors.length !== 0 &&
+          sponsors.map((sponsor) => (
+            <li>
+              <Link to={`sponsors/${sponsor.id}`} state={sponsor}>
+                {sponsor.name}{" "}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 };
 
 export default Home;
