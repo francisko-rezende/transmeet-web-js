5f542b85a56941c1fe9fd36533332ba1bce4f657
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-22 18:08:33 -0300

    feat: create route leading to user details

--- src/components/SponsorRoutes/SponsorRoutes.jsx
+++ src/components/SponsorRoutes/SponsorRoutes.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SponsorHome from "../../pages/SponsorHome";
+import UserDetails from "../../pages/UserDetails";
 
 const SponsorRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<SponsorHome />} />
-        {/* <Route path="/sponsors/:id" element={<SponsorDetails />} /> */}
+        <Route path="/usuario/:id" element={<UserDetails />} />
         {/* <Route path="/candidaturas" element={<SponsorshipRequests />} /> */}
         {/* <Route path="/requisições" element={<UserStats />} /> */}
         {/* <Route path="*" element={<NotFound />} /> */}
