dad0ec9e42871cf5e4c9f9c47d833e0906bcc3d0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-22 23:17:40 -0300

    feat: implement search

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,8 @@ const Home = () => {
     userData: { access_token, account },
   } = React.useContext(UserContext);
 
+  const [query, setQuery] = React.useState("");
+  const [searchResult, setSearchResult] = React.useState([]);
   const [sponsors, setSponsors] = React.useState([]);
 
   React.useEffect(() => {
@@ -25,19 +27,64 @@ const Home = () => {
     }
   }, []);
 
+  const searchForCompany = async (searchTerm) => {
+    const response = await api.get(`/sponsors/${searchTerm}`, {
+      headers: {
+        authorization: `Bearer ${access_token}`,
+      },
+    });
+    console.log(response);
+    setSearchResult(response.data);
+  };
+
   return (
     <>
       <h1>Home</h1>
-      <ul>
-        {sponsors.length !== 0 &&
-          sponsors.map((sponsor) => (
-            <li key={sponsor.id}>
-              <Link to={`sponsors/${sponsor.id}`} state={sponsor}>
-                {sponsor.name}{" "}
-              </Link>
-            </li>
-          ))}
-      </ul>
+      <form>
+        <label htmlFor="search">Buscar Empresa</label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            searchForCompany(query);
+          }}
+        >
+          Buscar
+        </button>
+      </form>
+      <div>
+        <h2>Resultado da busca</h2>
+        <ul>
+          {searchResult.length !== 0 &&
+            searchResult.map((result) => (
+              <li key={result.id}>
+                <Link to={`sponsors/${result.id}`} state={result}>
+                  {result.name}{" "}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Todas as empresas</h2>
+        <ul>
+          {sponsors.length !== 0 &&
+            sponsors.map((sponsor) => (
+              <li key={sponsor.id}>
+                <Link to={`sponsors/${sponsor.id}`} state={sponsor}>
+                  {sponsor.name}{" "}
+                </Link>
+              </li>
+            ))}
+        </ul>
+      </div>
 
       <h2>
         <Link to="candidaturas">Ver candidaturas</Link>
