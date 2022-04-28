b0a5f3ffe81b9ee46514beddcccb28242ddfe0ed
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-28 08:13:26 -0300

    fix: now actually track trans reg form styles

---
+++ src/components/TransRegistrationForm/TransRegistrationForm.styles.js
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components";
+
+export const TextArea = styled.textarea`
+  padding: 15px 23px;
+  max-height: 173px;
+  max-width: 470px;
+  border-width: 1px;
+  border-color: ${({ theme }) => theme.colors.typographyColors.gray};
+  border-style: solid;
+  border-radius: 4px;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 24px;
+
+  ${({ theme }) =>
+    css`
+      color: ${theme.colors.typographyColors.gray};
+    `}
+`;
