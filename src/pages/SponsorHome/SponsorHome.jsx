3506e7e563b64de16566ae3f2c9b36dd81991885
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-29 00:01:46 -0300

    feat: add styled comps to sponsor home and handle its errors

--- src/pages/SponsorHome/SponsorHome.jsx
+++ src/pages/SponsorHome/SponsorHome.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import * as S from "./SponsorHome.styles";
+import Card from "../../components/Card";
+import CardContainer from "../../components/CardContainer";
+import Header from "../../components/Header";
+import Loading from "../../components/Loading";
 import { UserContext } from "../../components/UserContext/UserContext";
 import api from "../../services/api";
 
 const getSponsorshipStatus = (accept) =>
   ({ [true]: "Aceito", [false]: "Recusado", [null]: "Não decidiu" }[accept]);
+
+const getMatchStatus = (accept) =>
+  ({ [true]: "match", [false]: "no match", [null]: "underway" }[accept]);
 
 const SponsorHome = () => {
   const {
@@ -12,38 +19,51 @@ const SponsorHome = () => {
   } = React.useContext(UserContext);
 
   const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
 
   React.useEffect(() => {
     const fetchUsers = async () => {
-      const response = await api.get(`/match/sponsors/${account.id}`, {
-        headers: {
-          authorization: `Bearer ${access_token}`,
-        },
-      });
-      setUsers(response.data);
+      try {
+        setIsLoading(true);
+        setError(false);
+        const response = await api.get(`/match/sponsors/${account.id}`, {
+          headers: {
+            authorization: `Bearer ${access_token}`,
+          },
+        });
+        setUsers(response.data);
+        console.log(response.data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchUsers();
-  }, []);
+  }, [access_token, account.id]);
 
   return (
-    <>
-      <h1>Home - Sponsor</h1>
-      <ul>
+    <S.Bg>
+      <S.HeaderWrapper>
+        <Header name={account.name} />
+      </S.HeaderWrapper>
+      <CardContainer>
+        {isLoading && <Loading />}
         {users.length !== 0 &&
           users.map((user) => (
             <li key={user.id}>
-              <p>Nome: {`${user.accounts_accountsTomatch_idUser.name}`}</p>
-              <p>Status: {getSponsorshipStatus(user.accept)}</p>
-              <p>
-                <Link to={`candidatos/${user.id}`} state={user}>
-                  booora
-                </Link>
-              </p>
+              <Card
+                to={`candidatos/${user.id}`}
+                state={user}
+                name={`${user.accounts_accountsTomatch_idUser.name}`}
+                matchStatus={getMatchStatus(user.accept)}
+              />
             </li>
           ))}
-      </ul>
-    </>
+      </CardContainer>
+    </S.Bg>
   );
 };
 
