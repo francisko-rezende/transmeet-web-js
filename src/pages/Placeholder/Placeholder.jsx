b24ddfdbe5108814752afe4f28e889ca71b692e9
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-29 00:00:42 -0300

    feat: make placeholder comp navigate user back to login

--- src/pages/Placeholder/Placeholder.jsx
+++ src/pages/Placeholder/Placeholder.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Placeholder = () => {
+  const navigate = useNavigate();
+
+  React.useEffect(() => navigate("/"));
+
   return <div style={{ background: "red" }}>Placeholder</div>;
 };
 
