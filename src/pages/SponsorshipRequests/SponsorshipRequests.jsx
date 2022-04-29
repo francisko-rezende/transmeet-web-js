d815ae9898ee70017807c4866d395ab5f3f0020f
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-04-29 00:02:24 -0300

    feat: add styled comps to sponsorship requests and handle its errors

--- src/pages/SponsorshipRequests/SponsorshipRequests.jsx
+++ src/pages/SponsorshipRequests/SponsorshipRequests.jsx
@@ -1,27 +1,48 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import CardContainer from "../../components/CardContainer";
+import Loading from "../../components/Loading";
 import { UserContext } from "../../components/UserContext/UserContext";
 import api from "../../services/api";
+import * as S from "./SponsorshipRequests.styles";
 
-const getSponsorshipStatus = (accept) =>
-  ({ [true]: "Aceito", [false]: "Recusado", [null]: "Não decidiu" }[accept]);
+// const getSponsorshipStatus = (accept) =>
+//   ({ [true]: "Aceito", [false]: "Recusado", [null]: "Não decidiu" }[accept]);
+
+const getMatchStatus = (accept) =>
+  ({ [true]: "match", [false]: "no match", [null]: "underway" }[accept]);
 
 const Home = () => {
+  const location = useLocation();
+
+  const previousLocation = location.pathname.split("/").slice(0, -1).join("/");
+
   const {
     userData: { access_token, account },
   } = React.useContext(UserContext);
 
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
   const [sponsorshipRequests, setSponsorshipRequests] = React.useState([]);
 
   React.useEffect(() => {
     const fetchSponsorshipRequests = async () => {
-      const response = await api.get(`/match/users/${account.id}`, {
-        headers: {
-          authorization: `Bearer ${access_token}`,
-        },
-      });
-      console.log(response);
+      try {
+        setIsLoading(true);
+        const response = await api.get(`/match/users/${account.id}`, {
+          headers: {
+            authorization: `Bearer ${access_token}`,
+          },
+        });
 
-      setSponsorshipRequests(response.data);
+        if (response.status === 200) {
+          setSponsorshipRequests(response.data);
+        }
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     if (account.typeId === 1) {
@@ -30,20 +51,31 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <h1>{account.name} - candidaturas</h1>
-      <ul>
+    <S.Bg>
+      <S.Container>
+        <S.Header>
+          <S.Link to={previousLocation}>
+            <S.LeftArrowIcon /> Voltar
+          </S.Link>
+          <S.Title>{account.name} - candidaturas</S.Title>
+        </S.Header>
+      </S.Container>
+      <CardContainer>
+        {isLoading && <Loading />}
         {sponsorshipRequests.length !== 0 &&
           sponsorshipRequests.map((request) => (
-            <li>
-              <p>
-                Nome: {`${request.accounts_accountsTomatch_idSponsor.name}`}
-              </p>
-              <p>Status: {getSponsorshipStatus(request.accept)}</p>
+            <li id={`${request.accounts_accountsTomatch_idSponsor.name}`}>
+              <S.SponsorshipCard
+                to={"#"}
+                // as="div"
+                name={`${request.accounts_accountsTomatch_idSponsor.name}`}
+                // id={`${request.accounts_accountsTomatch_idSponsor.name}`}
+                matchStatus={getMatchStatus(request.accept)}
+              />
             </li>
           ))}
-      </ul>
-    </>
+      </CardContainer>
+    </S.Bg>
   );
 };
 
